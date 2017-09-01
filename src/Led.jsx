52580db04299e12100dc966dc984bcb6f33093c6
Alex Anderson <alexanderson1993@gmail.com>
2017-09-01 13:59:15 +0000

    Updated to work with Raspberry Pi

--- src/Led.jsx
+++ src/Led.jsx
@@ -5,10 +5,16 @@ module.exports = class Led extends React.Component {
   constructor(props) {
     super(props);
     this._ledInstance = new five.Led(this.props.pin);
-  }
+    this.state = {on: props.on}  
+ }
+ componentDidMount(){
+	if (this.props.blink) {
+	setInterval(() => this.setState({on: !this.state.on}), this.props.interval || 500);
+	}
+}
   
   render() {
-  	if (this.props.on) {
+  	if (this.state.on) {
   		this._ledInstance.on();
   	} else {
   		this._ledInstance.off();
--- src/World.jsx
+++ src/World.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 const five = require('johnny-five');
+const Raspi = require('raspi-io');
 
 const Led = require('./Led');
 const LightSensor = require('./LightSensor');
@@ -15,8 +16,11 @@ module.exports = class World extends React.Component {
     }
 
     // Initialise arduino for this World
-    const arduino = new five.Board();
-    arduino.on('ready', () => this.setState({ready: true}));
+    const board = new five.Board({
+        io: new Raspi(),
+        repl:false
+    });
+    board.on('ready', () => this.setState({ready: true}));
 
     this._update = this._update.bind(this);
   }
@@ -31,11 +35,11 @@ module.exports = class World extends React.Component {
     if (this.state.ready) {
       worldElements =
         <div>
-          <Led pin={2} on={this.state.moisture > 500}/>
-          <Led pin={4} on={this.state.light < 130}/>
-          <LightSensor pin={'A1'} update={this._update}/>
+          <Led pin={7} on={true} blink/>
+          <Led pin={4} on={true}/>
+{/*          <LightSensor pin={'A1'} update={this._update}/>
           <MoistureSensor pin={'A2'} update={this._update}/>
-        </div>;
+  */}      </div>;
     }
 
     return (
